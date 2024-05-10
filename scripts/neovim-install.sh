e016056aa3d5a099fce537e8ee2515198274d0f1
BrownCarter <lilbrown3000@gmail.com>
2024-05-10 11:33:44 +0100

    fixed prompt and permission

--- scripts/neovim-install.sh
+++ scripts/neovim-install.sh
@@ -4,9 +4,10 @@ if command -v nvim &> /dev/null; then
     exit
 fi
 
-sudo apt-get install software-properties-common
-sudo apt-get install python-software-properties
-sudo add-apt-repository ppa:neovim-ppa/stable
-sudo apt-get update
-sudo apt-get install neovim
-sudo apt-get install python-dev python-pip python3-dev python3-pip
+sudo apt-get update -y
+sudo apt-get install software-properties-common -y
+sudo apt-get install python-software-properties -y
+sudo add-apt-repository ppa:neovim-ppa/stable -y
+sudo apt-get update -y
+sudo apt-get install neovim -y
+sudo apt-get install python-dev python-pip python3-dev python3-pip -y
--- scripts/zsh-install.sh
+++ scripts/zsh-install.sh
@@ -3,6 +3,7 @@
 if command -v nvim &> /dev/null; then
     exit
 fi
+
 sudo apt update
 sudo chsh -s "$(which zsh)"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" | yes
