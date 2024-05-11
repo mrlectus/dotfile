ba8bcf2d4703d98a9704a9a6b730253d20fd9d0f
BrownCarter <lilbrown3000@gmail.com>
2024-05-11 06:09:06 +0100

    fixed setup script

--- scripts/neovim-install.sh
+++ scripts/neovim-install.sh
@@ -5,9 +5,7 @@ if command -v nvim &> /dev/null; then
 fi
 
 sudo apt-get update -y
-sudo apt-get install software-properties-common -y
-sudo apt-get install python-software-properties -y
 sudo add-apt-repository ppa:neovim-ppa/stable -y
 sudo apt-get update -y
 sudo apt-get install neovim -y
-sudo apt-get install python-dev python-pip python3-dev python3-pip -y
+sudo apt-get install python-dev-is-python3 python-pip python3-dev python3-pip -y
