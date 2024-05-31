c1af73f113f4062cf4688679bf3ce4ef9d069f25
BrownCarter <lilbrown3000@gmail.com>
2024-05-31 04:07:53 +0100

    did a bunch of more stuff

--- .config/nvim/lua/config/format.lua
+++ .config/nvim/lua/config/format.lua
@@ -1,5 +1,3 @@
-local format_on_save = require("format-on-save")
-local formatters = require("format-on-save.formatters")
 -- Utilities for creating configurations
 local util = require("formatter.util")
 
@@ -124,18 +122,3 @@ vim.api.nvim_exec(
   ]],
   true
 )
-
-format_on_save.setup({
-  experiments = {
-    partial_update = true,
-  },
-  exclude_path_patterns = {
-    "/node_modules/",
-    ".local/share/nvim/lazy",
-  },
-  formatter_by_ft = {
-    typescript = formatters.prettierd,
-    typescriptreact = formatters.prettierd,
-    javascriptreact = formatters.prettierd,
-  },
-})
--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,64 +1,55 @@
 return {
-	{ "echasnovski/mini.nvim", version = "*" },
-	"p00f/clangd_extensions.nvim",
-	"github/copilot.vim",
-	{
-		"neovim/nvim-lspconfig",
-		inlay_hints = { enabled = true },
-	},
-	{ "elentok/format-on-save.nvim" },
-	"folke/lsp-colors.nvim",
-	"mfussenegger/nvim-dap",
-	{ "rcarriga/nvim-dap-ui", dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" } },
-	"nanozuki/tabby.nvim",
-	"leoluz/nvim-dap-go",
-	"folke/which-key.nvim",
-	"gko/vim-coloresque",
-	"nvim-tree/nvim-tree.lua",
-	{ "autozimu/LanguageClient-neovim", build = "bash install.sh" },
-	"hrsh7th/vim-vsnip-integ",
-	"nvim-tree/nvim-web-devicons",
-	{ "lukas-reineke/indent-blankline.nvim" },
-	{
-		"kylechui/nvim-surround",
-		version = "*", -- Use for stability; omit to use `main` branch for the latest features
-		event = "VeryLazy",
-		config = function()
-			require("nvim-surround").setup({
-				-- Configuration here, or leave empty to use defaults
-			})
-		end,
-	},
-	"mattn/emmet-vim",
-	"hrsh7th/cmp-vsnip",
-	"hrsh7th/vim-vsnip",
-	"nvim-telescope/telescope-media-files.nvim",
-	"mfussenegger/nvim-jdtls",
-	"mhartington/formatter.nvim",
-	"nanotee/sqls.nvim",
-	"lewis6991/impatient.nvim",
-	"nvim-lua/lsp-status.nvim",
-	"nvim-lua/plenary.nvim",
-	"nvim-lua/popup.nvim",
-	"kdheepak/lazygit.nvim",
-	"nvim-treesitter/playground",
-	"onsails/lspkind-nvim",
-	"udalov/kotlin-vim",
-	"hood/popui.nvim",
-	{
-		"akinsho/bufferline.nvim",
-		version = "v3.*",
-		dependencies = "nvim-tree/nvim-web-devicons",
-	},
-	"simrat39/rust-tools.nvim",
-	{ "akinsho/flutter-tools.nvim", dependencies = "nvim-lua/plenary.nvim" },
-	"simrat39/symbols-outline.nvim",
-	{
-		"ray-x/lsp_signature.nvim",
-		event = "VeryLazy",
-		opts = {},
-		config = function(_, opts)
-			require("lsp_signature").setup(opts)
-		end,
-	},
+  { "echasnovski/mini.nvim", version = "*" },
+  "p00f/clangd_extensions.nvim",
+  "github/copilot.vim",
+  {
+    "neovim/nvim-lspconfig",
+    inlay_hints = { enabled = true },
+  },
+  "folke/lsp-colors.nvim",
+  "mfussenegger/nvim-dap",
+  { "rcarriga/nvim-dap-ui",  dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" } },
+  "leoluz/nvim-dap-go",
+  "folke/which-key.nvim",
+  "gko/vim-coloresque",
+  "nvim-tree/nvim-tree.lua",
+  "nvim-tree/nvim-web-devicons",
+  { "lukas-reineke/indent-blankline.nvim" },
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({
+        -- Configuration here, or leave empty to use defaults
+      })
+    end,
+  },
+  "mattn/emmet-vim",
+  "hrsh7th/cmp-vsnip",
+  "mfussenegger/nvim-jdtls",
+  "mhartington/formatter.nvim",
+  "nanotee/sqls.nvim",
+  "nvim-lua/lsp-status.nvim",
+  "nvim-lua/plenary.nvim",
+  "hood/popui.nvim",
+  "kdheepak/lazygit.nvim",
+  "nvim-treesitter/playground",
+  "onsails/lspkind-nvim",
+  "udalov/kotlin-vim",
+  {
+    "akinsho/bufferline.nvim",
+    version = "v3.*",
+    dependencies = "nvim-tree/nvim-web-devicons",
+  },
+  "simrat39/rust-tools.nvim",
+  { "akinsho/flutter-tools.nvim",         dependencies = "nvim-lua/plenary.nvim" },
+  {
+    "ray-x/lsp_signature.nvim",
+    event = "VeryLazy",
+    opts = {},
+    config = function(_, opts)
+      require("lsp_signature").setup(opts)
+    end,
+  },
 }
